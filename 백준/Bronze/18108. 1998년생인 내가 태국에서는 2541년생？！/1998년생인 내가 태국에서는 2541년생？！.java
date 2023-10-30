b85a24877cbc2b9043c1ca880c28886f5d77d4d2
Bijou <140521784+pyominmin@users.noreply.github.com>
2023-10-30 21:21:27 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = (a - 543);
+		System.out.println(b);
+		
+	}
+}
